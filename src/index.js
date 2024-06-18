0944b25e18f5b34e9c959d86aa7c813a6adc178b
Silva-Mo <thediamondheart890@gmail.com>
2024-06-19 00:48:17 +0400

    Add page main elements with basic CSS for testing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './normalize.css';
 import './styles.css';
+import logo from './restaurant-outline-svgrepo-com.svg'
+
+const logoContainer = document.querySelector('.logo-container');
+const imgLogo = new Image();
+const txtLogo = document.querySelector('.logo-container h1')
+
+imgLogo.src = logo;
+logoContainer.insertBefore(imgLogo, txtLogo);
